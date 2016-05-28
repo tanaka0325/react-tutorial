f64db69952476604c1b3ca07c17b192f3970b691
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-05-28 11:15:49 +0900

    commentlist, commentform作成

--- public/js/tutorial.js
+++ public/js/tutorial.js
@@ -8,6 +8,26 @@ var CommentBox = React.createClass({
     }
 });
 
+var CommentList = React.createClass({
+    render: function() {
+        return (
+            <div className="commentList">
+                Hello, world! I am a CommentList.
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            <div className="commentForm">
+                Hello, world! I am a CommentForm.
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
     <CommentBox />,
     document.getElementById('content')
